653c2f8d3e03acaf4d34f5d4b367fc689a3cfc32
Bruno Monteiro <bunomonteiro@gmail.com>
2015-12-20 21:16:16 -0300

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,38 @@ var minifyCss = require('gulp-minify-css');
 var plumber = require('gulp-plumber');
 var rename = require('gulp-rename');
 var concat = require('gulp-concat');
+var header = require('gulp-header');
+var size = require('gulp-size');
 var del = require('del');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
+var pkg = require('./package.json');
+var banner = ['/**',
+  ' * <%= pkg.name %> - <%= pkg.description %>',
+  ' * @author <%= pkg.author %>',
+  ' * @version v<%= pkg.version %>',
+  ' * @link <%= pkg.homepage %>',
+  ' * @license <%= pkg.license %>',
+  ' */',
+  ''].join('\n');
 
 gulp.task('clean', del.bind(null, ['dist']));
 
-gulp.task('serve', ['build'], function() {
+gulp.task('compile', ['clean'], function() {
+  return gulp.src(['src/styles/*.scss', '!src/styles/index.scss'])
+    .pipe(plumber())
+    .pipe(sass.sync({
+      outputStyle: 'expanded',
+      precision: 10,
+      includePaths: ['.']
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('dist/'))
+    .pipe(reload({
+      stream: true
+    }));
+});
+
+gulp.task('serve', ['compile'], function() {
   browserSync({
     notify: false,
     port: 9000,
@@ -29,44 +54,30 @@ gulp.task('serve', ['build'], function() {
     'src/styles/*.css'
   ]).on('change', reload);
 
-  gulp.watch('src/styles/**/*.scss', ['build']);
+  gulp.watch('src/styles/**/*.scss', ['compile']);
 });
 
-gulp.task('minify', function() {
-  gulp.src('dist/*.css')
-    .pipe(minifyCss({
-      compatibility: '*'
-    }))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('dist'));
-
-  gulp.src(['dist/reset.css', 'dist/grid.css'])
+gulp.task('bundle', ['compile'], function(){
+  return gulp.src(['dist/reset.css', 'dist/grid.css'])
     .pipe(concat('grid.bundle.css'))
-    .pipe(gulp.dest('dist'))
-    .pipe(minifyCss({
-      compatibility: '*'
-    }))
-    .pipe(rename({
-      suffix: '.min'
-    }))
+    .pipe(gulp.dest('dist'));
+})
+
+gulp.task('minify', ['bundle'], function() {
+  return gulp.src('dist/*.css')
+    .pipe(minifyCss({ compatibility: '*' }))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build', ['clean'], function() {
-  return gulp.src(['src/styles/*.scss', '!src/styles/index.scss'])
-    .pipe(plumber())
-    .pipe(sass.sync({
-      outputStyle: 'expanded',
-      precision: 10,
-      includePaths: ['.']
-    }).on('error', sass.logError))
-    .pipe(gulp.dest('dist/'))
-    .pipe(reload({
-      stream: true
-    }));
+gulp.task('header', ['minify'], function() {
+  return gulp.src(['./dist/*.css', '!./dist/reset.css','!./dist/reset.min.css'])
+    .pipe(header(banner, { pkg : pkg } ))
+    .pipe(gulp.dest('./dist/'));
 });
-gulp.task('default', ['build'], function() {
-  gulp.start('minify');
+
+gulp.task('build', ['compile', 'bundle', 'minify', 'header'], function() {
+  return gulp.src('./dist/grid.min.css').pipe(size({title: 'build', gzip: true}));
 });
+
+gulp.task('default', ['clean', 'build']);
